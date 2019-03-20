a127120b4bb88e8e70f48fb484f9e3e386d930fc
Daniel Vitor Ruiz <ruiz.vitor@gmail.com>
2019-03-20 15:15:17 -0300

    motor testing
    Signed-off-by: Daniel Vitor Ruiz <ruiz.vitor@gmail.com>

--- app/src/main/java/com/dji/FPVDemo/VideoStream.java
+++ app/src/main/java/com/dji/FPVDemo/VideoStream.java
@@ -4,11 +4,16 @@ import android.graphics.ImageFormat;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
 import android.os.AsyncTask;
+import android.os.Handler;
 
+
+import com.dji.FPVDemo.internal.ModuleVerificationUtil;
+import com.dji.FPVDemo.internal.controller.DJISampleApplication;
 
 import org.apache.commons.logging.Log;
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBufferOutputStream;
+import org.ros.concurrent.CancellableLoop;
 import org.ros.internal.message.MessageBuffers;
 import org.ros.message.MessageListener;
 import org.ros.namespace.GraphName;
@@ -21,6 +26,15 @@ import org.ros.node.topic.Subscriber;
 import java.io.ByteArrayOutputStream;
 import java.nio.ByteOrder;
 
+import dji.common.error.DJIError;
+import dji.common.flightcontroller.Attitude;
+import dji.common.flightcontroller.FlightControllerState;
+import dji.common.flightcontroller.imu.IMUState;
+import dji.common.util.CommonCallbacks;
+import dji.keysdk.FlightControllerKey;
+import dji.sdk.flightcontroller.Compass;
+import dji.sdk.flightcontroller.FlightController;
+import dji.sdk.products.Aircraft;
 import std_msgs.String;
 
 import static org.jboss.netty.buffer.ChannelBuffers.copiedBuffer;
@@ -33,6 +47,9 @@ public class VideoStream extends AbstractNodeMain {
     Publisher<String> publisher;
     //    Publisher<sensor_msgs.Image> publisherImg;
     Publisher<sensor_msgs.CompressedImage> publisherImg;
+    Compass compass = null;
+    FlightController flightController;
+    FlightControllerState flightControllerState;
 
     @Override
     public GraphName getDefaultNodeName() {
@@ -47,7 +64,6 @@ public class VideoStream extends AbstractNodeMain {
         Subscriber<String> subscriber = connectedNode.newSubscriber("chatter", std_msgs.String._TYPE);
 
         publisher = connectedNode.newPublisher("chatterResponse", std_msgs.String._TYPE);
-//        publisherImg = connectedNode.newPublisher("chatterImg", sensor_msgs.Image._TYPE);
         publisherImg = connectedNode.newPublisher("camera/compressed", sensor_msgs.CompressedImage._TYPE);
 
 
@@ -59,7 +75,128 @@ public class VideoStream extends AbstractNodeMain {
             }
         });
 
+//        imu = new IMUState();
+
+        if (ModuleVerificationUtil.isFlightControllerAvailable()) {
+            flightController = ((Aircraft) DJISampleApplication.getProductInstance()).getFlightController();
+//            setIMUCallback();
+//            initCompass();
+
+            flightControllerState = flightController.getState();
+
+            flightController.turnOnMotors(new CommonCallbacks.CompletionCallback() {
+                @Override
+                public void onResult(DJIError error) {
+
+                    if (error == null) {
+                        android.util.Log.d(TAG, "turnOnMotors Succeeded");
+
+                        (new Handler()).postDelayed(mStopMotor, 5000);//
+
+                    } else {
+                        android.util.Log.e(TAG, error.getDescription());
+                    }
+
+                }
+            });
+
+        }
+
+
+
+//        connectedNode.executeCancellableLoop(new CancellableLoop() {
+//            private int sequenceNumber;
+//
+//            @Override
+//            protected void setup() {
+//                sequenceNumber = 0;
+//            }
+//
+//            @Override
+//            protected void loop() throws InterruptedException {
+////                std_msgs.String str = publisher.newMessage();
+////                str.setData("Hello world! " + sequenceNumber);
+////                publisher.publish(str);
+////                sequenceNumber++;
+//
+//                flightControllerState = flightController.getState();
+//
+//                Attitude att = flightControllerState.getAttitude();
+//
+//                android.util.Log.d(TAG, "att.pitch:" + att.pitch);
+//                android.util.Log.d(TAG, "att.yaw:" + att.yaw);
+//                android.util.Log.d(TAG, "att.roll:" + att.roll);
+//
+//                android.util.Log.d(TAG, "getTakeoffLocationAltitude:" + flightControllerState.getTakeoffLocationAltitude());
+//
+//                android.util.Log.d(TAG, "velx:" + flightControllerState.getVelocityX());
+//                android.util.Log.d(TAG, "vely:" + flightControllerState.getVelocityY());
+//                android.util.Log.d(TAG, "velz:" + flightControllerState.getVelocityZ());
+//
+////                android.util.Log.d(TAG, "north:" + getCompassHeading());
+//                Thread.sleep(100);
+//            }
+//        });
+
+
     }
+
+    private Runnable mStopMotor = new Runnable() {
+        @Override
+        public void run() {
+            flightController.turnOffMotors(new CommonCallbacks.CompletionCallback() {
+                @Override
+                public void onResult(DJIError error) {
+
+                    if (error == null) {
+                        android.util.Log.d(TAG, "turnOffMotors Succeeded");
+                    } else {
+                        android.util.Log.e(TAG, error.getDescription());
+                    }
+
+                }
+            });
+        }
+    };
+
+
+//    void setIMUCallback() {
+//
+//        flightController.setIMUStateCallback(new IMUState.Callback() {
+//
+//            @Override
+//            public void onUpdate(IMUState state) {
+//
+//                android.util.Log.d(TAG, "IMU ID:"+ state.getIndex());
+//                android.util.Log.d(TAG, "Is connected:"+ state.isConnected());
+//                android.util.Log.d(TAG, "AccelerometerState:"+ state.getAccelerometerState());
+//                android.util.Log.d(TAG, "AccelerometerValue:"+ state.getAccelerometerValue());
+//                android.util.Log.d(TAG, "getGyroscopeState:"+ state.getGyroscopeState());
+//                android.util.Log.d(TAG, "getGyroscopeValue:"+ state.getGyroscopeValue());
+//
+//            }
+//
+//        });
+//
+//    }
+
+
+//    void initCompass() {
+//        if (ModuleVerificationUtil.isFlightControllerAvailable()) {
+//            flightController = ((Aircraft) DJISampleApplication.getProductInstance()).getFlightController();
+//
+//            if (ModuleVerificationUtil.isCompassAvailable()) {
+//                compass = flightController.getCompass();
+//            }
+//        }
+//    }
+//
+//    float getCompassHeading() {
+//        if (compass != null)
+//            return compass.getHeading();
+//        else
+//            return 0;
+//    }
 
     public void pubMessage(java.lang.String msg) {
         std_msgs.String str = publisher.newMessage();
